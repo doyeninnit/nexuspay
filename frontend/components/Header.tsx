fccb80c9aa29966735beb01a24cd59838d9c082a
Nashons <agatenashons@gmail.com>
2023-09-27 07:25:19 +0300

    change header content and add scanner

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,60 +1,61 @@
 
 
+// import { MdNotifications, MdCenterFocusStrong } from 'react-icons/md'; // Import the icons you want to use
 // import { useAuth } from '../contexts/AuthContext';
 
 // const Header = () => {
 //     const { user } = useAuth();
 
 //     return (
-//       <header className="bg-gradient-to-r from-black to-purple-900 p-4">
-//         <div className="container mx-auto">
-//           <div className="flex justify-between items-center">
-//             <div className="text-white font-bold">NexusPay</div>
+//         <header className="bg-gradient-to-r from-black to-purple-900 p-4">
+//             <div className="container mx-auto">
+//                 <div className="flex justify-between items-center">
+//                     <div className="text-white font-bold">
+//                         Welcome, Anon
+//                     </div>
 
-//             {/* Check if the user exists and display the phoneNumber and walletAddress */}
-//             {user ? (
-//               <div className="text-white">
-//                 <div>Phone: {user.phoneNumber}</div>
-//                 {user.walletAddress && <div>Wallet: {user.walletAddress}</div>}
-//               </div>
-//             ) : (
-//               <div className="text-white">Account</div>
-//             )}
-//           </div>
-//         </div>
-//       </header>
+//                     {user ? (
+//                         <div className="text-white flex items-center space-x-4">
+//                             <MdNotifications size={24} /> {/* Notification Icon */}
+//                             <MdCenterFocusStrong size={24} /> {/* Scan Icon */}
+//                             {/* You can wrap the above icons in buttons or links if needed */}
+//                         </div>
+//                     ) : null}
+//                 </div>
+//             </div>
+//         </header>
 //     );
 // }
 
 // export default Header;
 
+
+import { MdNotifications, MdCenterFocusStrong } from 'react-icons/md'; 
 import { useAuth } from '../contexts/AuthContext';
+import QRScanner from './QRScanner'; // Make sure to import the scanner component
+import { useState } from 'react';
 
 const Header = () => {
   const { user } = useAuth();
+  const [showScanner, setShowScanner] = useState(false);
 
   return (
     <header className="bg-gradient-to-r from-black to-purple-900 p-4">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <div className="text-white font-bold">
-            <img src="/icon-192x192.png" alt="NexusPay Logo" className="w-6 h-6 inline-block mr-2" />
-            Afpay                    </div>
+            Welcome, Anon
+          </div>
 
           {user ? (
             <div className="text-white flex items-center space-x-4">
-              <span>Hello, User</span> {/* Assuming user object has firstName */}
-              <button >
-                Logout
+              <MdNotifications size={24} /> {/* Notification Icon */}
+              <button onClick={() => setShowScanner(!showScanner)}>
+                <MdCenterFocusStrong size={24} /> {/* Scan Icon */}
               </button>
-              {/* Or a dropdown menu with more account options */}
+              {showScanner && <QRScanner />}
             </div>
-          ) : (
-            <div className="text-white">
-              Account
-              {/* Links to login or register if user isn't logged in */}
-            </div>
-          )}
+          ) : null}
         </div>
       </div>
     </header>
@@ -62,5 +63,3 @@ const Header = () => {
 }
 
 export default Header;
-
-
