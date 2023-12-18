8ca6bf1402d42d44244107ca9f6b204a099cdd61
Nashons <agatenashons@gmail>
2023-12-18 18:59:36 +0300

    Add security, logging and other middlewares

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -6,7 +6,9 @@ import { ChainId } from "@biconomy/core-types";
 import { IPaymaster, BiconomyPaymaster } from "@biconomy/paymaster";
 
 
-export const provider = new providers.JsonRpcProvider("https://rpc.ankr.com/polygon_mumbai")
+// export const provider = new providers.JsonRpcProvider("https://rpc.ankr.com/polygon_mumbai")
+export const provider = new providers.JsonRpcProvider("https://polygon-mumbai.g.alchemy.com/v2/aMTaEiyjF4cjCbq_mF4Gvpktw-IgrscR")
+
 export const tokenAddress = '0xEE49EA567f79e280E4F1602eb8e6479d1Fb9c8C8'
 export const tokenContract = new ethers.Contract(tokenAddress, ERC20ABI, provider);
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,65 @@
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import bodyParser from 'body-parser';
+
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+
 import authRoutes from './routes/authRoutes';
 import businessRoutes from './routes/businessRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import usdcRoutes from './routes/usdcRoutes';
-import {connect } from './config/database'
+import { connect } from './config/database';
 
 const app = express();
 const PORT = 8000;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.json());
+// Security middlewares
+app.use(helmet());
 
+// CORS middleware for allowing cross-origin requests
+app.use(cors());
+
+// Body parser middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Compression middleware to compress response bodies
+app.use(compression());
+
+// Morgan middleware for logging HTTP requests
+app.use(morgan('dev'));
+
+// Rate Limiting to prevent brute force attacks
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // Limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+
+// Route middlewares
 app.use('/api/auth', authRoutes);
 app.use('/api/business', businessRoutes);
 app.use('/api/token', tokenRoutes);
-app.use('/api/usdc', usdcRoutes); 
+app.use('/api/usdc', usdcRoutes);
 
-
-
+// Database connection
 connect().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 });
 
+
+// Global error handling middleware
+const errorHandler: ErrorRequestHandler = (err, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send({ message: 'Internal Server Error' });
+};
+
+app.use(errorHandler);
