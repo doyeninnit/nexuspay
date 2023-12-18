1d7ed8ebaf0cefc70e62f47307bbe14125aef593
Nashons <agatenashons@gmail>
2023-12-18 17:32:28 +0300

    make jwt middleware handle errors better

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,5 +1,7 @@
+
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+
 
 declare global {
     namespace Express {
@@ -8,15 +10,28 @@ declare global {
       }
     }
   }
-  
+
+const JWT_SECRET = process.env.JWT_SECRET || 'zero'; // Use environment variable for JWT secret
+
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
 
-    if (token == null) return res.sendStatus(401);
+    if (!token) {
+        return res.status(401).json({ message: "No token provided" });
+    }
 
-    jwt.verify(token, 'zero', (err, user) => {
-        if (err) return res.sendStatus(403);
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) {
+            // Provide more specific messages based on the type of error
+            if (err.name === 'JsonWebTokenError') {
+                return res.status(401).json({ message: "Invalid token" });
+            } else if (err.name === 'TokenExpiredError') {
+                return res.status(401).json({ message: "Token expired" });
+            } else {
+                return res.status(403).json({ message: "Unauthorized access" });
+            }
+        }
         req.user = user;
         next();
     });
